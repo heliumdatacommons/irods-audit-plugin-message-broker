0077964932a84d2e52bff4c9034fb3e6ce0e849d
Daniel Sikes <danielsikes@rciadmins-MacBook-Pro.local>
2018-04-12 12:31:31 -0400

    enabled durablity to both consumer and publisher

--- message_broker/utils/RabbitMQInit.py
+++ message_broker/utils/RabbitMQInit.py
@@ -20,7 +20,7 @@ class RabbitMQInit():
             raise Exception("unable to consume. No queue provided.")
         
         logger.debug("consuming from queue %s" % (queue))
-        self.channel.queue_declare(queue=queue)
+        self.channel.queue_declare(queue=queue, durable=True)
         self.channel.basic_consume(callback, queue=queue, no_ack=no_ack)
         self.channel.start_consuming()
     
@@ -29,7 +29,7 @@ class RabbitMQInit():
             raise Exception("unable to publish. No queue provided.")
 
         logger.debug("publishing to queue %s" % (queue))
-        self.channel.queue_declare(queue=queue)   
+        self.channel.queue_declare(queue=queue, durable=True)   
         self.channel.basic_publish(exchange='', routing_key=queue, body=message)
 
     def close(self):
